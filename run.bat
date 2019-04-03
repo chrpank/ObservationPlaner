0c4a7e7eb5388a528edf68fa012f6c010d19b5f8
Christian Pankratz <chrpank@googlemail.com>
2019-04-03 21:14:10 +0200

    add batch file example

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
-python run.py "para/site_para.txt" "para/moon_para.txt" "data/Moon2019.txt" "data/Sun2019.txt"
+python run.py "para/site_para.txt" "para/moon_para.txt"    "data/Moon2019.txt"     "data/Sun2019.txt"
+python run.py "para/site_para.txt" "para/mars_para.txt"    "data/Mars2019.txt"     "data/Sun2019.txt"
+python run.py "para/site_para.txt" "para/jupiter_para.txt" "data/Jupiter2019.txt"  "data/Sun2019.txt"
 pause
