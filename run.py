40178a3471ac019019b95a5d624f0e2394c8960c
Christian Pankratz <chrpank@googlemail.com>
2019-04-04 20:14:02 +0200

    refined output

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import argparse
 debug_mode = False
 
 # parse commandline arguments
-# ***************************
 arg_parser = argparse.ArgumentParser()
 
 arg_parser.add_argument("sit_para_path",
@@ -15,38 +14,36 @@ arg_parser.add_argument("obj_data_path",
 arg_parser.add_argument("sun_data_path",
                         help="The sun data filepath.")
 
+
+print("observation event calculation")
 print("sit_para_path = " + arg_parser.parse_args().sit_para_path)
 print("obj_para_path = " + arg_parser.parse_args().obj_para_path)
 print("obj_data_path = " + arg_parser.parse_args().obj_data_path)
 print("sun_data_path = " + arg_parser.parse_args().sun_data_path)
-print("\n")
 
 # parse site settings
-# *******************
 site_para_file = open(arg_parser.parse_args().sit_para_path)
 site_min_azim = site_para_file.readline()[0:8]
 site_max_azim = site_para_file.readline()[0:8]
 site_para_file.close()
 
+print("observation view data")
 print("site_min_azim = " + site_min_azim)
 print("site_max_azim = " + site_max_azim)
-print("\n")
 
 # parse parameter file
-# ********************
 obj_para_file = open(arg_parser.parse_args().obj_para_path)
 obj_min_alti = obj_para_file.readline()[0:8]
 sun_min_dist = obj_para_file.readline()[0:8]
 sun_max_alti = obj_para_file.readline()[0:8]
 obj_para_file.close()
 
+print("object condition data")
 print("obj_min_alti = " + obj_min_alti)
 print("sun_min_dist = " + sun_min_dist)
 print("sun_max_alti = " + sun_max_alti)
-print("\n")
 
 # calculate obs events
-# ********************
 obj_data_file = open(arg_parser.parse_args().obj_data_path)
 obj_data_lins = obj_data_file.readlines()[8:]
 
@@ -54,7 +51,8 @@ sun_data_file = open(arg_parser.parse_args().sun_data_path)
 sun_data_lins = sun_data_file.readlines()[8:]
 
 print("table of observation events")
-print("***************************")
+
+event_not_found = True
 
 for index in range(len(obj_data_lins)):
 
@@ -71,10 +69,8 @@ for index in range(len(obj_data_lins)):
         print("obj_alti = " + obj_alti)
         print("sun_azim = " + sun_azim)
         print("sun_alti = " + sun_alti)
-        print("\n")
 
     # check conditions
-    # ****************
     cond_obj_azim = bool(float(obj_azim) > float(site_min_azim) and
                          float(obj_azim) < float(site_max_azim))
     cond_obj_alti = bool(float(obj_alti) > float(obj_min_alti))
@@ -82,3 +78,9 @@ for index in range(len(obj_data_lins)):
 
     if (cond_obj_azim and cond_obj_alti and cond_sun_alti):
         print(obj_line.rstrip())
+        event_not_found = False
+
+if event_not_found is True:
+    print("No events found.")
+
+print("end of output")
